c08c4d1d2e08f008d09a84d324f6a0cc8bef30d7
Irfan Khairul <irfankhairuln@gmail.com>
2024-04-16 16:56:23 +0800

    Fetch the genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,42 +1,32 @@
 import { useEffect, useState } from "react"
 import apiClient from "../services/api-client"
 import { CanceledError } from "axios"
-// faking api to save request...
-import fakeApi from "../assets/games.json"
+import fakeApi from "../services/fake-api"
 
-export interface Platform {
+interface Genre {
   id: number
   name: string
-  slug: string
 }
 
-export interface Game {
-  id: number
-  name: string
-  background_image: string
-  parent_platforms: { platform: Platform }[]
-  metacritic: number
-}
-
-interface FetchGamesResponse {
+interface FetchGenresResponse {
   count: number
-  results: Game[]
+  results: Genre[]
 }
 
-export default function useGames() {
-  const [games, setGames] = useState<Game[]>([])
+export default function useGenres() {
+  const [genres, setGenres] = useState<Genre[]>([])
   const [error, setError] = useState("")
   const [isLoading, setLoading] = useState(false)
-  
+
   useEffect(() => {
     const controller = new AbortController()
 
     setLoading(true)
     apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
       .then((res) => {
         setLoading(false)
-        setGames(res.data.results)
+        setGenres(res.data.results)
       })
       .catch((err) => {
         if (err instanceof CanceledError) return
@@ -45,7 +35,7 @@ export default function useGames() {
           console.error("GET method failed. Revert to fake-api.ts")
           console.error("To resolve, please check BaseURL in api-client.ts")
           setLoading(false)
-          return setGames(fakeApi)
+          return setGenres(fakeApi)
         }
         setError(err.message)
         setLoading(false)
@@ -54,5 +44,5 @@ export default function useGames() {
     return () => controller.abort()
   }, [])
 
-  return { games, error ,isLoading}
+  return { genres, error, isLoading }
 }
