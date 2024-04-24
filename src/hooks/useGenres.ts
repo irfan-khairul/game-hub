acebc00cc951009f384a174d3c94cd24c6196ba6
Irfan Khairul <44071833+irfan-khairul@users.noreply.github.com>
2024-04-24 13:25:38 +0800

    Use react-query to fetch genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,9 @@
 // import useData from "./useData"
-import genres from "../data/genres"
 // import genres from "../assets/genres.json" // for faking api
+import genres from "../data/genres"
+import { useQuery } from "@tanstack/react-query"
+import apiClient from "../services/api-client"
+import { FetchResponse } from "./useData"
 
 export interface Genre {
   id: number
@@ -8,7 +11,17 @@ export interface Genre {
   image_background: string
 }
 
-// const useGenres = () => useData<Genre>("/genres")
-const useGenres = () => ({ data: genres, isLoading: false, error: null })
+// const useGenres = () => useData<Genre>("/genres") // use Data hook with axios
+// const useGenres = () => ({ data: genres, isLoading: false, error: null }) // use local data
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Genre>>("/genres")
+        .then((response) => response.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: { count: genres.length, results: genres },
+  })
 
 export default useGenres
