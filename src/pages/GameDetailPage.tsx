657594f2f192d741f39f33c51997c5ea9e591f66
Irfan Khairul <44071833+irfan-khairul@users.noreply.github.com>
2024-04-30 21:50:56 +0800

    Build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,11 +3,12 @@ import { useParams } from "react-router-dom"
 import ExpandableText from "../components/ExpandableText"
 import GameAtrributes from "../components/GameAtrributes"
 import useGame from "../hooks/useGame"
+import GameTrailer from "../components/GameTrailer"
 
 const GameDetailPage = () => {
   const { slug } = useParams()
   const { data: game, isLoading, error } = useGame(slug!)
-  console.log(game)
+  // console.log(game)
 
   if (isLoading) return <Spinner />
   if (error || !game) throw error
@@ -16,6 +17,7 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAtrributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   )
 }
